8eb5c604aecaa32b7adff5f0886801b9ebbc7583
zeenat <zeenat@alobhadev.com>
2022-03-15 14:38:20 +0530

    updated code again

--- server/Routes/UserAuthenticationServices.js
+++ server/Routes/UserAuthenticationServices.js
@@ -1,8 +1,16 @@
 const Router= require('express');
 const express = require('express')
 const router = express.Router()
-const db = express.require('./Utilities/database')
 
+// DB related codes
+const MongoClient = require('mongodb').MongoClient
+const myurl = 'mongodb://localhost:27017';
+var db;
+MongoClient.connect(myurl, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('zeenatdb')
+
+});
 
 //middleware that is specific router
 //LOGIN API
